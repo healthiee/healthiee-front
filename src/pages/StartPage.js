398aed66d9010d0a8e83e63923702b1c7414189b
miraclee1226 <shl6534@naver.com>
2023-10-13 14:20:39 +0900

    feat: Startpage 개발

---
+++ src/pages/StartPage.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import styled from "styled-components";
+import mainLogo from '../assets/images/mainLogo.png'
+
+const MainLogo = styled.img`
+  @media ${({theme}) => theme.mobileSize.mobile} {
+    width: 300px;
+    height: 60px;
+    margin: 130px 30px 270px;
+  }
+`
+const Button = styled.button`
+ @media ${({theme}) => theme.mobileSize.mobile} {
+    width: 200px;
+    height: 60px;
+    margin: 0 80px 120px;
+    font-size: ${({theme}) => theme.fontSize.xlg};
+    font-weight: ${({theme}) => theme.fontWeight.bold};
+    border-radius: 30px;
+  }
+`
+const StartPage = () => {
+  const navigate = useNavigate();
+  return (
+    <React.Fragment>
+      <MainLogo src={mainLogo} alt="healthiee main logo" />
+      <Button onClick={() => navigate('/enteremail')}>시작하기</Button>
+    </React.Fragment>
+  );
+};
+
+export default StartPage;
