78b23d98f7f683f361674a9f7491dc34ce308572
miraclee1226 <shl6534@naver.com>
2023-10-13 14:09:50 +0900

    design: ThemeProvider 적용

---
+++ src/styles/theme.js
@@ -0,0 +1,33 @@
+const mobileSize = {
+  mobile: `screen and (max-width: 360px)`,
+};
+
+const fontSize = {
+  sm: '12px',
+  md: '16px',
+  lg: '20px',
+  xlg: '24px',
+};
+
+const fontWeight = {
+  regular: 400,
+  bold: 700,
+};
+
+const colors = {
+  gray: '#717171',
+  lightGray: '#EFEFEF',
+  red: '#FF0000',
+  lightRed: '#FFE0E0',
+  orange: '#FF9F4E',
+  lighOrange: '#FFC493',
+};
+
+const theme = {
+  mobileSize,
+  fontWeight,
+  fontSize,
+  colors,
+};
+
+export default theme;
