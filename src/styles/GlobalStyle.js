ba851af847915fade7b7e4572ff729b09c1aa4d0
miraclee1226 <shl6534@naver.com>
2023-10-13 14:09:23 +0900

    design: GlobalStyle 적용

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,52 @@
+import { createGlobalStyle } from "styled-components";
+import reset from 'styled-reset';
+
+const GlobalStyle = createGlobalStyle`
+  ${reset}
+    * {
+      box-sizing: border-box;
+    }
+
+    body {
+      font-family: 'Noto Sans KR', sans-serif;
+      line-height: 1.2;
+    }
+
+    html, body, div, span, h1, h2, h3, h4, h5, h6, p, 
+    a, dl, dt, dd, ol, ul, li, form, label, table{
+      margin: 0;
+      padding: 0;
+      border: 0;
+      font-size: 12px;
+      vertical-align: baseline;
+      color: '#000000';
+    }
+
+    a {
+      color: inherit;
+      font-size: 12px;
+    }
+
+    button {
+      border: none;
+      background-color: #EFEFEF;
+      border-radius: 20px;
+      font-size: 16px;
+    }
+
+    input {
+      border: none;
+      outline: none;
+    }
+
+    ol, ul, li {
+      list-style: none;
+    }
+
+    img {
+      display: block;
+      height: 100%;
+    }
+`;
+
+export default GlobalStyle;
