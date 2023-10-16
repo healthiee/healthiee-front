93bfe0648565782cf4083bba64cddf04e7e5b4d8
miraclee1226 <shl6534@naver.com>
2023-10-16 19:21:30 +0900

    feat: BrowserRouter와 createBrowserRouter 사용
    React Router v6.4에 추가된 방식 사용

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider  } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/GlobalStyle';
 import theme from './styles/theme';
@@ -6,19 +6,19 @@ import SplashScreen from './pages/SplashScreen';
 import StartPage from './pages/StartPage';
 import EmailLogin from './pages/EmailAuth/EmailLogin';
 
+const router = createBrowserRouter([
+  {path: '/', element: <SplashScreen />},
+  {path: '/startpage', element: <StartPage />},
+  {path: '/emaillogin', element: <EmailLogin />}
+]);
+
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<SplashScreen />} />
-          <Route path='/startpage' element={<StartPage />} />
-          <Route path='/emaillogin' element={<EmailLogin />} />
-        </Routes>
-      </BrowserRouter>
+      <RouterProvider router={router} />
     </ThemeProvider>
-  );
+  )
 }
 
 export default App;
