9652101400da12161fb9754a6aa031a501ad5d81
miraclee1226 <shl6534@naver.com>
2023-11-09 15:26:27 +0900

    feat: Notification 팝업창 구현

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,34 +1,52 @@
 import styles from './Home.module.css';
-import { Fragment } from 'react';
-import {ReactComponent as Tune} from '../../assets/images/tune.svg';
-import {ReactComponent as Notification} from '../../assets/images/notification.svg';
-import logo from '../../assets/images/logo.png'
+import { Fragment, useState, useEffect, useRef } from 'react';
+import { ReactComponent as Tune } from '../../assets/images/tune.svg';
+import { ReactComponent as Notification } from '../../assets/images/notification.svg';
+import logo from '../../assets/images/logo.png';
 import Contents from './Contents';
-import { Link } from 'react-router-dom'
+import NotificationPopup from '../../pages/Notification';
 
-const Home  = () => {
-  return(
+const Home = () => {
+  const [popupVisible, setPopupVisible] = useState(false);
+  const swipeRef = useRef(null);
+
+  const showNotification = () => {
+    setPopupVisible(true);
+  };
+  const showHomePage = () => {
+    setPopupVisible(false);
+  };
+
+  useEffect(() => {
+  swipeRef.current.classList.toggle(styles.showNotificaton) 
+  }, [popupVisible]);
+
+  return (
     <Fragment>
+      <div className={styles.NotificationPopup} ref={swipeRef}>
+        {popupVisible && <NotificationPopup onClose={showHomePage} />}
+      </div>
+
       <header className={styles.header}>
         <div>
           <img src={logo} alt="logo" />
         </div>
-        <Link to="/notification">
-          <Notification />
-        </Link>
+        <div className={styles.notifiaction}>
+          <Notification onClick={showNotification} />
+        </div>
       </header>
 
       <div className={styles.tune}>
-        <Tune/>
+        <Tune />
       </div>
 
       <div className={styles.contents}>
-        <Contents/>
-        <Contents/>
-        <Contents/>
+        <Contents />
+        <Contents />
+        <Contents />
       </div>
     </Fragment>
-  )
+  );
 };
 
 export default Home;
