59429210e7397b43304c4607b046cc589758719f
miraclee1226 <shl6534@naver.com>
2023-11-12 20:27:57 +0900

    feat: onShowCommentPage props <Home />으로 전달

--- src/components/HomePage/Contents.js
+++ src/components/HomePage/Contents.js
@@ -1,7 +1,7 @@
 import styles from './Contents.module.css';
-import {ReactComponent as AttachFile} from '../../assets/images/attachFile.svg';
-import {ReactComponent as Heart} from '../../assets/images/heart.svg';
-import { useState } from 'react';
+import { ReactComponent as AttachFile } from '../../assets/images/attachFile.svg';
+import { ReactComponent as Heart } from '../../assets/images/heart.svg';
+import { useState} from 'react';
 
 const Contents = (props) => {
 
@@ -19,42 +19,52 @@ const Contents = (props) => {
 
   const heartStyle = heart ? styles.active : styles.heart;
 
+  const showCommentPage = () => {
+    props.onShowCommentPage();
+  }
+
   return (
-  <article className={styles.article}>
-    <div className={styles.content_info}>
-      <div className={styles.profile}>
-        <div className={styles.profile_img}>
-        <img src={props.post.profileImg} alt="profile_img" />
+    <article className={styles.article}>
+      <div className={styles.content_info}>
+        <div className={styles.profile}>
+          <div className={styles.profile_img}>
+            <img src={props.post.profileImg} alt="profile_img" />
+          </div>
+          <div className={styles.profile_box}>
+            <h1>{props.post.nickname}</h1>
+            <div className={styles.profile_tags}>
+              {props.post.tags.map(tag => <div className={styles.profile_tag} key={tag.name} style={{ backgroundColor: `${tag.color}` }}>{tag.name}</div>)}
+            </div>
+          </div>
         </div>
-        <div className={styles.profile_box}>
-          <h1>{props.post.nickname}</h1>
-          <div className={styles.profile_tags}>
-            {props.post.tags.map(tag => <div className={styles.profile_tag} key={tag.name} style={{backgroundColor:`${tag.color}`}}>{tag.name}</div>)}
+
+        <div className={styles.icons}>
+          <AttachFile style={{ marginBottom: '10px' }} />
+          <div onClick={heartClickHandler}>
+            <Heart className={heartStyle} />
+            <p>{props.post.love}</p>
           </div>
         </div>
       </div>
 
-      <div className={styles.icons}>
-        <AttachFile style={{marginBottom:'10px'}}/>
-        <div onClick={heartClickHandler}>
-          <Heart className={heartStyle}/>
-          <p>{props.post.love}</p>
+      <div className={styles.content_img}>
+        <img src={props.post.postImg} alt="content_img" />
+      </div>
+
+      <div className={styles.comment}>
+        <div className={styles.comment_head}>
+          <h2>{props.post.created_at}</h2>
+          <h2 onClick={showCommentPage} >
+            댓글 {props.post.comments}개
+          </h2>
+        </div>
+        <p>{props.post.body}</p>
+        <div>
+          
         </div>
       </div>
-    </div>
-
-    <div className={styles.content_img}>
-      <img src={props.post.postImg} alt="content_img" />
-    </div>
-
-    <div className={styles.comment}>
-      <div className={styles.comment_head}>
-        <h2>{props.post.created_at}</h2>
-        <h2>댓글 {props.post.comments}개</h2>
-      </div>
-      <p>{props.post.body}</p>
-    </div>
-  </article>)
+      
+    </article>)
 };
 
 export default Contents;
