85d40eceee5e0fc63980b258494761d969b50ba2
miraclee1226 <shl6534@naver.com>
2023-11-20 22:57:39 +0900

    refactor: axios 인스턴스 생성

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const API = axios.create({
-    baseURL: 'http://prod.healthiee.net',
+    baseURL: 'http://prod.healthiee.net/',
+    withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
         'X-Request-ID': '{UUID}',
