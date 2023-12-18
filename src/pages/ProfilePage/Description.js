a17b71839365b491346f850f49fcfee1d473e548
miraclee1226 <shl6534@naver.com>
2023-12-18 23:52:53 +0900

    feat: 피드 썸네일 사진 표시

--- src/pages/ProfilePage/Description.js
+++ src/pages/ProfilePage/Description.js
@@ -6,45 +6,49 @@ const DescriptionWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   padding: 20px 22px;
-`
+`;
+
 const ImageThumbnail = styled.img`
   width: 104px;
   height: 104px;
-  background-image: url(${props => props.postImg});
   border-radius: 25px;
-`
+`;
+
 const Description = () => {
-  const [postImg, setPostImg] = useState([]);
+  const [postImages, setPostImages] = useState([]);
 
   useEffect(() => {
-    const fetchImg = async() => {
+    const fetchImages = async () => {
       try {
-        const response = await axios.get('http://prod.healthiee.net/v1/posts',{
+        const response = await axios.get('http://prod.healthiee.net/v1/posts', {
           headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwic3ViIjoiNzM2Y2Y0NTQtMjgxOC00ZmQ5LWEwNzctMzAwYjZmNWVmZTY0IiwiaWF0IjoxNjk5ODUyMjU4LCJleHAiOjE3ODYyNTIyNTh9.4-aiUFJpIEmhUlehg5YPVHPYjTQ7GP-2jTV63JYqXho`,
-          }
-        })
-        setPostImg(response.data.data.content);
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwic3ViIjoiNzM2Y2Y0NTQtMjgxOC00ZmQ5LWEwNzctMzAwYjZmNWVmZTY0IiwiaWF0IjoxNjk5ODUyMjU4LCJleHAiOjE3ODYyNTIyNTh9.4-aiUFJpIEmhUlehg5YPVHPYjTQ7GP-2jTV63JYqXho`, // Replace with your actual token
+          },
+        });
+        const posts = response.data.data.content;
+        const images = posts.flatMap((post) =>
+        post.medias.map((media) => media.url)
+        );
+        console.log(posts);
+        console.log(images);
+        setPostImages(images);
       } catch (err) {
-      console.log(err);
-    }
-  }
-    fetchImg();
+        console.error(err);
+      }
+    };
+
+    fetchImages();
   }, []);
 
   return (
     <DescriptionWrapper>
-      {postImg && postImg.map((img, i) => {
-        return(
-          <React.Fragment key={postImg.id}>
-              <ImageThumbnail 
-                src={img.medias.url}
-              />
-          </React.Fragment>
-        )
-      })}
+      {postImages.map((imgUrl, i) => (
+        <React.Fragment key={i}>
+          <ImageThumbnail src={imgUrl} alt={`Image ${i}`} />
+        </React.Fragment>
+      ))}
     </DescriptionWrapper>
-    )
+  );
 };
 
 export default Description;
