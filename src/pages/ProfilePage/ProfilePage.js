121d701babc25ddd52b366b5d9f411847ef00f55
miraclee1226 <shl6534@naver.com>
2023-12-18 13:38:30 +0900

    design: 단계별 뱃지 적용

--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,12 +1,42 @@
 import { NavLink, Outlet } from "react-router-dom";
 import styles from './ProfilePage.module.css';
+import styled from 'styled-components';
 import { useState } from "react";
 import {ReactComponent as Menu} from '../../assets/images/menu.svg';
 import {ReactComponent as Description} from '../../assets/images/description.svg';
 import {ReactComponent as EventAvailable} from '../../assets/images/eventAvailable.svg';
+import {ReactComponent as BadgeLevel1} from '../../assets/images/BadgeIllustration/badgeLevel1.svg';
+import {ReactComponent as BadgeLevel2} from '../../assets/images/BadgeIllustration/badgeLevel2.svg';
+import {ReactComponent as BadgeLevel3} from '../../assets/images/BadgeIllustration/badgeLevel3.svg';
+import {ReactComponent as BadgeLevel4} from '../../assets/images/BadgeIllustration/badgeLevel4.svg';
+import {ReactComponent as BadgeLevel5} from '../../assets/images/BadgeIllustration/badgeLevel5.svg';
 import defaultProfile from '../../assets/images/defaultProfile.png';
 import Setting from './SettingPage/Setting';
 
+const Badge1 = styled(BadgeLevel1)`
+  position: absolute;
+  width: 53px;
+  height: 57px;
+  margin: 44px 0 0 13px;
+`
+const Badge2 = styled(BadgeLevel2)`
+  position: absolute;
+  width: 53px;
+  height: 57px;
+  margin: 44px 0 0 13px;
+`
+const Badge3 = styled(BadgeLevel3)`
+  position: absolute;
+  margin-top: 30px;
+`
+const Badge4 = styled(BadgeLevel4)`
+  position: absolute;
+  margin-top: 30px;
+`
+const Badge5 = styled(BadgeLevel5)`
+  position: absolute;
+  margin-top: 30px;
+`
 const profileDummy = {
   profileimg: defaultProfile,
   follower: 657,
@@ -24,6 +54,7 @@ const profileDummy = {
 const ForthPage = () => {
 
   const [setting, setSetting] = useState(false);
+  const [popup, setPopup] = useState(false);
 
   const settingHandler = () => {
     setSetting(true);
@@ -37,11 +68,16 @@ const ForthPage = () => {
         <div className={styles.header}>
           <h1>{profileDummy.nickname}</h1>
           <div><Menu onClick={settingHandler} style={{cursor:'pointer'}}/></div>
-          {setting && <Setting/>}
+          {setting && <Setting onPopup={setPopup}/>}
         </div>
 
         <div className={styles.profile}>
           <div className={styles.profile_img}>
+            <Badge1 />
+            {/* <Badge2 />
+            <Badge3 />
+            <Badge4 />
+            <Badge5 /> */}
             <img src={profileDummy.profileimg} alt="profile_img" />
           </div>
           <div className={styles.follow}>
