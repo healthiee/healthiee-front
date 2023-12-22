a311605c16259497523c98024089121ef0cacda6
miraclee1226 <shl6534@naver.com>
2023-12-22 11:45:48 +0900

    style: 콘솔 제거

--- src/pages/ProfilePage/Description.js
+++ src/pages/ProfilePage/Description.js
@@ -22,15 +22,13 @@ const Description = () => {
       try {
         const response = await axios.get('http://prod.healthiee.net/v1/posts', {
           headers: {
-            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwic3ViIjoiNzM2Y2Y0NTQtMjgxOC00ZmQ5LWEwNzctMzAwYjZmNWVmZTY0IiwiaWF0IjoxNjk5ODUyMjU4LCJleHAiOjE3ODYyNTIyNTh9.4-aiUFJpIEmhUlehg5YPVHPYjTQ7GP-2jTV63JYqXho`, // Replace with your actual token
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJ0eXBlIjoiYWNjZXNzX3Rva2VuIiwic3ViIjoiNzM2Y2Y0NTQtMjgxOC00ZmQ5LWEwNzctMzAwYjZmNWVmZTY0IiwiaWF0IjoxNjk5ODUyMjU4LCJleHAiOjE3ODYyNTIyNTh9.4-aiUFJpIEmhUlehg5YPVHPYjTQ7GP-2jTV63JYqXho`,
           },
         });
         const posts = response.data.data.content;
         const images = posts.flatMap((post) =>
         post.medias.map((media) => media.url)
         );
-        console.log(posts);
-        console.log(images);
         setPostImages(images);
       } catch (err) {
         console.error(err);
