8da5bc6b3deea5b27e265a5246785ba3b7042f15
miraclee1226 <shl6534@naver.com>
2023-12-26 20:55:17 +0900

    refactor: 컴포넌트 가독성 개선 및 미디어 쿼리 제거

--- src/pages/StartPage.js
+++ src/pages/StartPage.js
@@ -1,31 +1,32 @@
-import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import styled from "styled-components";
 import mainLogo from '../assets/images/mainLogo.png'
+import styled from "styled-components";
 
 const MainLogo = styled.img`
-  @media ${({theme}) => theme.mobileSize.mobile} {
-    width: 300px;
-    height: 60px;
-    margin: 130px 30px 270px;
-  }
-`
+  width: 300px;
+  height: 60px;
+  margin: 130px 30px 270px;
+`;
+
 const Button = styled.button`
- @media ${({theme}) => theme.mobileSize.mobile} {
-    width: 200px;
-    height: 60px;
-    margin: 0 80px 120px;
-    font-size: ${({theme}) => theme.fontSize.xlg};
-    font-weight: ${({theme}) => theme.fontWeight.bold};
-    border-radius: 30px;
-  }
-`
+  width: 200px;
+  height: 60px;
+  margin: 0 80px 120px;
+  font-size: ${({ theme }) => theme.fontSize.xlg};
+  border-radius: 30px;
+`;
+
 const StartPage = () => {
   const navigate = useNavigate();
+
+  const handleStartClick = () => {
+    navigate('/emaillogin')
+  };
+
   return (
     <>
       <MainLogo src={mainLogo} alt="healthiee main logo" />
-      <Button onClick={() => navigate('/emaillogin')}>시작하기</Button>
+      <Button onClick={handleStartClick}>시작하기</Button>
     </>
   );
 };
